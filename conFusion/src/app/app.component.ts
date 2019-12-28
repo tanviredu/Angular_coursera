b8699ed02d8aeb258a4b6a9b881b82c5072d54b6
mechanic <ornobtanvir.git@gmail.com>
2019-12-28 21:06:41 +0600

    added

--- conFusion/src/app/app.component.ts
+++ conFusion/src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // you can see this Component is select
+  // the whole app-root
+  // so changing its template and scss
+  // will affect the whole app
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
