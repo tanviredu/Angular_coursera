da72e64a924e2f7f0ead0bba6374365bac18dc29
mechanic <ornobtanvir.git@gmail.com>
2019-12-28 20:12:36 +0600

    Configuring Angular

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -3,12 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+// heer add the additional module here that we installed
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import 'hammerjs';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
+// in here we register the module
+// we register the material toolbar we can add it
+// on a component
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
